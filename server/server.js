b55a7597c1d1beafb95d1ac1363c6ae03416b3e8
prakriti1605 <prakritijain1205@gmail.com>
2026-07-30 20:55:14 +0530

    deploying backend on render

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import { connectDB } from "./config/db.js";
 const PORT = process.env.PORT || 8000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0" ,() => {
     console.log(`Server running on port ${PORT}`);
   });
 });
