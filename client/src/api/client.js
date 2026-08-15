8f66029090ee5c2fe7d9396fc2590d43cd606da1
prakriti1605 <prakritijain1205@gmail.com>
2026-08-15 16:29:39 +0530

    added correct url now

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://project-mgmnt-eu2f.onrender.com",
+  baseURL:"https://project-mgmnt-eu2f.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
